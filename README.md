1e5954f2b6649334bf3f99e67f47404d803aa056
biendo <aime@awelty.com>
2017-10-19 11:05:55 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# EVENTSLIB
+> Events library classes.
+
+Some events classes: ActionListener, ActionEvent, EventListenerAggregate, EventObject etc...
+
+## Installation
+
+Node:
+
+```sh
+npm install eventslib --save
+```
+
+## Meta
+
+Aime – abiendo@gmail.com
+
+Distributed under the MIT license. See ``LICENSE`` for more information.
---
+++ index.js
@@ -0,0 +1,33 @@
+/** @babel */
+import {
+    ActionListener,
+    ActionEvent} from './lib/action';
+import {EventListenerAggregate} from './lib/aggregate';
+import {EventListener,EventListenerProxy,EventObject} from './lib/event';
+import {EventListenerList} from './lib/list';
+import {
+    PropertyChangeListener,
+    PropertyChangeListenerProxy,
+    PropertyChangeEvent,
+    IndexedPropertyChangeEvent,
+    PropertyChangeSupport} from './lib/properties';
+
+export {
+    ActionListener,
+    ActionEvent,
+
+    EventListenerAggregate,
+
+    EventListener,
+    EventListenerProxy,
+    EventObject,
+
+    EventListenerList,
+
+    PropertyChangeListener,
+    PropertyChangeListenerProxy,
+    PropertyChangeEvent,
+    IndexedPropertyChangeEvent,
+    PropertyChangeSupport
+
+}
